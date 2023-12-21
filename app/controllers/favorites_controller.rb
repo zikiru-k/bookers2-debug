c643b2a13f7687c7aa0ea6c6dbf71ff32ee300a3
zikiru-k <149685941+zikiru-k@users.noreply.github.com>
2023-12-21 15:12:16 +0000

    [Update] いいね機能を非同期通信化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,21 +3,21 @@ class FavoritesController < ApplicationController
     # BookモデルからFavoriteモデルの:book_idと同じパラメーター(id)を探して
     # `book`変数には、検索された結果が代入されます。
     # これにより、IDを指定して特定の`Book`レコードを取得することができます。
-    book  = Book.find(params[:book_id])
-    
+    @book  = Book.find(params[:book_id])
+
     # current_user.favorites：現在ログインしているユーザーに関連付けられた`favorites`という関連モデルを取得するため
     # new：新しい`favorite`オブジェクトを作成
     # book_id: book.id：作成された`favorite`オブジェクトの`book_id`属性に`book`のIDを設定しています。
     # `book`は2行目で取得した`Book`モデルのインスタンスであり、そのIDを`favorite`オブジェクトの`book_id`属性に関連付けることができます。
-    favorite = current_user.favorites.new(book_id: book.id)
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_back(fallback_location: books_path(book))
+    # redirect_to request.referer
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_back(fallback_location: books_path(book))
+    # redirect_to request.referer
   end
 end
