1464e1324850665bda15a7d23bc11164b2840f4e
zikiru-k <149685941+zikiru-k@users.noreply.github.com>
2023-12-26 15:26:50 +0000

    [Add] 課題8b chart.jsを使い、過去 7 日分の投稿数グラフ化

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_19_122956) do
+ActiveRecord::Schema.define(version: 2023_12_25_143817) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(version: 2023_12_19_122956) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "view_count"
   end
 
   create_table "chats", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 課題8b
+# 5人のユーザーを作成
+5.times do |n|
+  user = User.create(
+    name: "user#{n+1}", # ユーザー名を設定 ("user1" から "user5")
+    email: "test#{n+1}@test.com", # ユーザーのメールアドレスを設定
+    password: "123456", # パスワードを一律で設定
+  )
+  if user.valid?
+    puts "User #{n+1} created" # ユーザーの作成をコンソールに出力
+  else
+    puts "Error creating user #{n+1}: #{user.errors.full_messages.join(', ')}" # エラー表示
+  end
+end
+
+# それぞれのユーザーに対して、5つの本を作成
+User.all.each do |user|
+  5.times do |n|
+    book = Book.create(
+      title: "本#{n+1}", # 本のタイトルを設定
+      body: "サンプル投稿です#{n+1}", # 本の本文を設定
+      user_id: user.id,  # 本をユーザーに関連付け
+      created_at: Time.current - rand(10).day # 本の作成日時を設定（過去10日間からランダムに選択）
+    )
+    if book.valid?
+      puts "Book for User #{user.id} created" # 本の作成をコンソールに出力
+    else
+      puts "Error creating book for User #{user.id}: #{book.errors.full_messages.join(', ')}" # エラー表示
+    end
+  end
+end
